d40b15e782bad84bd36f21b3e9483c29023dadf6
nhanpro <nhunguyenthanhnhu83@gmail.com>
2025-04-25 09:43:12 +0700

    frame

---
+++ quanlybanhang/src/main/java/com/project/quanlybanhang/QuanlybanhangApplication.java
@@ -0,0 +1,14 @@
+package com.project.quanlybanhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlybanhangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlybanhangApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ quanlybanhang/src/test/java/com/project/quanlybanhang/QuanlybanhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quanlybanhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlybanhangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
